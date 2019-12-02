1e882f09de1bc65568a759b4ce6c5d585505c71b
Adam Staveley <adam.staveley@shareandcharge.com>
2019-12-02 11:30:24 +0100

    additional tariff interfaces

--- src/models/ocpi/tariffs.ts
+++ src/models/ocpi/tariffs.ts
@@ -13,41 +13,31 @@ export interface ITariff {
     tariff_alt_url?: string
     min_price?: IPrice
     max_price?: IPrice
-    elements: Array<{
-        price_components: IPriceComponent[]
-        restrictions?: {
-            start_time?: string
-            end_time?: string
-            start_date?: string
-            end_date?: string
-            min_kwh?: number
-            max_kwh?: number
-            min_current?: number
-            max_current?: number
-            min_power?: number
-            max_power?: number
-            min_duration?: number
-            max_duration?: number
-            day_of_week?: string[]
-            reservation?: string
-        }
-    }>
+    elements: ITariffElement
     start_date_time?: string
     end_date_time?: string
-    energy_mix?: {
-        is_green_energy: boolean
-        energy_sources?: Array<{
-            source: string
-            percentage: number
-        }>
-        environ_impact?: Array<{
-            category: string
-            amount: number
-        }>
-        supplier_name?: string
-        energy_product_name?: string
-    }
+    energy_mix?: IEnergyMix
     last_updated: string
+}
+
+export interface ITariffElement {
+    price_components: IPriceComponent[]
+    restrictions?: {
+        start_time?: string
+        end_time?: string
+        start_date?: string
+        end_date?: string
+        min_kwh?: number
+        max_kwh?: number
+        min_current?: number
+        max_current?: number
+        min_power?: number
+        max_power?: number
+        min_duration?: number
+        max_duration?: number
+        day_of_week?: string[]
+        reservation?: string
+    }
 }
 
 export interface IPriceComponent {
@@ -56,3 +46,18 @@ export interface IPriceComponent {
     vat?: number
     step_size: number
 }
+
+export interface IEnergyMix {
+    is_green_energy: boolean
+    energy_sources?: Array<{
+        source: string
+        percentage: number
+    }>
+    environ_impact?: Array<{
+        category: string
+        amount: number
+    }>
+    supplier_name?: string
+    energy_product_name?: string
+}
+
